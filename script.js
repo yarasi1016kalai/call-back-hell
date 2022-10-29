521a3ec742de995967ba0a51fde69c23f6369a18
yarasi1016kalai <107178706+yarasi1016kalai@users.noreply.github.com>
2022-10-29 21:59:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+setTimeout(() => {
+     document.getElementById("countdown").innerHTML =10;
+     setTimeout(() => {
+       document.getElementById("countdown").innerHTML =9; 
+     setTimeout(() => {
+       document.getElementById("countdown").innerHTML =8; 
+      setTimeout(() => {
+       document.getElementById("countdown").innerHTML =7; 
+    setTimeout(() => {
+      document.getElementById("countdown").innerHTML =6; 
+     setTimeout(() => {
+    document.getElementById("countdown").innerHTML =5; 
+     setTimeout(() => {
+    document.getElementById("countdown").innerHTML =4; 
+     setTimeout(() => {
+    document.getElementById("countdown").innerHTML =3; 
+     setTimeout(() => {
+    document.getElementById("countdown").innerHTML =2; 
+     setTimeout(() => {
+     document.getElementById("countdown").innerHTML =1; 
+      setTimeout(() => {
+        document.getElementById("countdown").innerHTML = "Happy Independence day" ; 
+          
+    },1000);
+
+          
+    },1000);
+                                      
+     },1000);
+                                  
+     },1000);
+                              
+    },1000);
+                          
+      },1000);
+                      
+      },1000);
+                  
+     },1000);
+            
+        },1000);
+        
+    },1000);
+},1000);
